cef4c1b34259a7b751248880355336655658721d
useredsa <emilio.dominguezs@um.es>
2021-04-06 19:48:04 +0200

    Añadida bibliografía

--- propuesta-inicial-de-mejora/gen.sh
+++ propuesta-inicial-de-mejora/gen.sh
@@ -2,4 +2,6 @@
 
 mkdir -p out/ &&
 pdflatex --synctex=1 -output-directory=out src/main.tex &&
+biber out/main &&
+pdflatex --synctex=1 -output-directory=out src/main.tex &&
 pdflatex --synctex=1 -output-directory=out src/main.tex
